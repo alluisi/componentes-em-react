e1053541ddaa52e9cc92226537b5f1c626f52082
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-09-04 18:27:31 -0300

    Hello World

---
+++ react-dio/src/App.js
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// export default function App () {
+//   return (
+//     <h1>Helli World</h1>
+//   )
+// }
